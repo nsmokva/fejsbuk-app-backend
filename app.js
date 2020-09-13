c98e38bb15ce3f5c542bf095ee932765ef5ddc95
nsmokva <nikolina.smokrovic@gmail.com>
2020-09-13 22:07:19 +0200

    wip

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ var jsonParser = bodyParser.json()
 const port = 4000
 
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema
 mongoose.connect('mongodb+srv://nsmokva:nsmokva12@cluster0-wgok7.mongodb.net/fejsbuk-app?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 
-const User = mongoose.model('User', { 
+const UserSchema = new Schema ({ 
     email: String,
     password: String,
     firstName: String,
@@ -21,14 +22,30 @@ const User = mongoose.model('User', {
     introduction: String,
 });
 
-const Status = mongoose.model('Status', {
+const CommentSchema = new Schema({
+    comment: String,
+    ownerId: String,
+    ownerFirstName: String,
+    ownerLastName: String,
+    date: Object,
+});
+
+const StatusSchema = new Schema({
     status: String,
     ownerId: String,
     ownerFirstName: String,
     ownerLastName: String,
     date: Object,
-    likes: Array
-})
+    likes: [String],
+    comments: [CommentSchema]
+});
+
+
+const User = mongoose.model('User', UserSchema);
+
+const Status = mongoose.model('Status', StatusSchema)
+
+
 
 //const firstUser = new User({ email: 'marko', password: '1234', firstName:"Marko", lastName:"Smokrovic"});
 //firstUser.save().then(() => console.log('Nikolina in db'));
@@ -48,20 +65,17 @@ app.get('/backend/login', (req, res) => {
     .then(function(result){
         //if there is no user with given username in db, status code 404
         if (result===null){
-            console.log('status code 404')
             res.status(404).send('status code 404')
         //if there is user with given username in db but given password is not matching password from db, status code 401 (unauthorized)
         }else if (result.password !== req.query.password){
-            console.log("status code 401")
             res.status(401).send("status code 401")     
         //if there is given user in db and given password matches password fom db, status code 200               
         }else{
-            console.log('status code 200')
             res.status(200).send(result)
         }
     })
     .catch(function(err){
-            console.log(err);
+            console.log('*******1st error********', err);
     })
 })
 // 2. get user based on user id
@@ -70,16 +84,14 @@ app.get('/backend/user', (req, res) => {
     .then(function(result){
         //if there is no user with given username in db, status code 404
         if (result===null){
-            console.log('status code 404')
             res.status(404).send('status code 404')    
         //if there is given user in db and given password matches password fom db, status code 200               
         }else{
-            console.log('status code 200')
             res.status(200).send(result)
         }
     })
     .catch(function(err){
-            console.log(err);
+            console.log('*******2nd error********', err);
     })
 })
 
@@ -89,16 +101,14 @@ app.get('/backend/users', (req, res) => {
     .then(function(result){
         //if there are no users in db, status code 404
         if (result===null){
-            console.log('status code 404')
             res.status(404).send('status code 404')    
         //if there are users in db, status code 200               
         }else{
-            console.log('status code 200')
             res.status(200).send(result)
         }
     })
     .catch(function(err){
-            console.log(err);
+            console.log('*******3rd error********', err);
     })
 })
 
@@ -107,14 +117,14 @@ app.post('/backend/user/userdata', jsonParser, (req, res) =>{
     var filter = {_id: req.body.id}
     var update = { city: req.body.city, email2: req.body.email2, job: req.body.job, education: req.body.education}
     var user = User.findOneAndUpdate(filter, update)
-    .then(result => console.log("Ok"))
-    .catch(error => console.log(error))
-    
-    var updateduser = User.findOne(filter)
     .then(result => {
-        res.status(200).send(result)
+        var updateduser = User.findOne(filter)
+        .then(result => {
+            res.status(200).send(result)
+        })
+        .catch(error => console.log('*******4th error********', error))
     })
-    .catch(error => console.log(error))
+    .catch(error => console.log('*******5th error********', error))
 })
 
 //update userdata/introduction in db
@@ -122,33 +132,79 @@ app.post('/backend/user/userdata/introduction', jsonParser, (req, res) =>{
     var filter = {_id: req.body.id}
     var update = { introduction: req.body.introduction}
     var user = User.findOneAndUpdate(filter, update)
-    .then(result => console.log('ok'))
-    .catch(error => console.log(error))
-    
-    var updateduser = User.findOne(filter)
     .then(result => {
-        res.status(200).send(result)
+        var updateduser = User.findOne(filter)
+        .then(result => {
+            res.status(200).send(result)
+        })
+        .catch(error => console.log('*******6th error********', error))
     })
-    .catch(error => console.log(error))
+    .catch(error => console.log('*******7th error********', error))    
 })
 
 //save new status to db
 app.post('/backend/statuses', jsonParser, (req, res) =>{
-    console.log('backend nwe status req.body', req.body)
     var newStatus = new Status({ status: req.body.status, ownerId: req.body.ownerId, ownerFirstName: req.body.ownerFirstName, ownerLastName: req.body.ownerLastName, date: req.body.date});
     newStatus.save()
     .then(result =>{
         res.status(200).send(result)
     })
     .catch(error =>{
-        console.log(error)
+        console.log('*******8th error********', error)
     })
 })
 
 //update status in db (add likes)
+app.post('/backend/statuses/likes', jsonParser, (req, res) =>{
+    console.log(req.body.id)
+    var filter = {_id: req.body.id}
+    var user = Status.findOne(filter)
+    .then(result => {
+        console.log('result of pressing like: ', result)
+        const found = result.likes.findIndex(element => element === req.body.like);
+        console.log('found is: ', found)
+        if (found == -1){
+            result.likes.push(req.body.like)
+        } else{
+            result.likes.splice(found, 1)
+        }
+        result.save().then(() => {
+        var updatedUserLike = Status.findOne(filter)
+        .then(result => {
+            console.log('result updated like: ', result)
+            res.status(200).send(result)
+        })
+        })
+        .catch(error => console.log('*******9th error********', error))
+    })
+    .catch(error => console.log('*******10th error********', error))
+})
 
+//update status in db (add comments)
+app.post('/backend/statuses/comments', jsonParser, (req, res) =>{
+    console.log(req.body)
+    var filter = {_id: req.body.statusId}
+    Status.findOne(filter)
+        .then(result => {
+            console.log('result of pressing comment: ', result) 
+            result.comments.unshift({
+                comment: req.body.comment,
+                ownerId: req.body.ownerId,
+                ownerFirstName: req.body.ownerName,
+                ownerLastName: req.body.ownerLastName,
+                date: req.body.date
+            })
 
-
+            result.save().then(() => {
+                Status.findOne(filter)
+                .then(result => {
+                    res.status(200).send(result)
+                })
+            })
+        
+        })
+        .catch(error => console.log('*******10th error********', error))
+})
 
 
 
@@ -157,23 +213,18 @@ app.post('/backend/statuses', jsonParser, (req, res) =>{
 
 //get all satuses from db
 app.get('/backend/statuses', (req, res) =>{
-    console.log('REQ BODY')
-    console.log(req.query.ownerId)
     var statuses = Status.find({ownerId: req.query.ownerId})
     .then(function(result){
         //if there are no statuses in db, status code 404
         if (result===null){
-            console.log('status code 404')
             res.status(404).send('status code 404')    
         //if there are statuses in db, status code 200               
         }else{
-            console.log('status code 200 statusesssssssssssssssssssss')
-            console.log(result)
             res.status(200).send(result)
         }
     })
     .catch(function(err){
-            console.log(err);
+            console.log('*******11th error********', err);
     })
 })
 
@@ -185,7 +236,7 @@ app.delete('/backend/statuses/:id', (req, res) => {
         res.send('status deleted')
     })
     .catch(function(err){
-            console.log(err);
+            console.log('*******12th error********', err);
     })
     
  })
